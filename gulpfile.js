019ff7c6b58a7f9abd1d78750c77ce51809c1eb3
German Fros <german.fros551@gmail.com>
2023-07-07 16:56:44 -0300

    Criando Hero e importando fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps')
+const imagemin = require('gulp-imagemin')
 
 function compilaSass() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./build/styles'))
+    return gulp.src('src/styles/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('build/styles'))
+}
 
-
-    }
+function comprimeImg() {
+    return gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/images'))
+}
 
 exports.default = function() {
-    gulp.watch('./src/styles/**/*.scss', {ignoreInitial: false}, gulp.series(compilaSass))
+    gulp.watch('src/styles/**/*.scss', {ignoreInitial: false}, gulp.series(compilaSass))
 }
+
+exports.img = comprimeImg
